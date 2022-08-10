dbe2eaa4ee8dbd34810cd3b11495c18d569e0268
hyuntae <bht9011@gmail.com>
2022-08-11 08:53:07 +0900

    study before work 20220811

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,7 +1,47 @@
+import { GetStaticPaths, GetStaticProps } from "next";
+import Head from "next/head";
 import React from "react";
+import { getAllPostsIds, getPostData } from "../../lib/post";
+import postStyle from "../../styles/Post.module.css";
 
-function Post() {
-  return <div>[id]</div>;
+function Post({
+  postData,
+}: {
+  postData: {
+    title: string;
+    date: string;
+    contentHtml: string;
+  };
+}) {
+  return (
+    <div className={postStyle.container}>
+      <Head>{postData.title}</Head>
+      <article>
+        <h1>{postData.title}</h1>
+        <div>{postData.date}</div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }}></div>
+      </article>
+    </div>
+  );
 }
 
 export default Post;
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = getAllPostsIds();
+  // [{params: id: 'pre-rendering}, {}]
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  console.log("params", params);
+  const postData = await getPostData(params.id as string);
+  return {
+    props: {
+      postData,
+    },
+  };
+};
